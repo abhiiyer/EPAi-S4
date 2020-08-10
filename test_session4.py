c14255a86e8460be273640d103059c15e1af8e9d
abhiiyer <abhishekr.iyer88@gmail.com>
2020-08-10 16:57:01 +0530

    Added more test cases

--- test_session4.py
+++ test_session4.py
@@ -1,125 +1,174 @@
-import pytest , os , session4 , inspect , re , random
-from decimal import Decimal
+import pytest
+import random
+import string
+import session4
+import os
+import inspect
+import re
+import math
+from math import isclose
+import decimal
 
+number_1 =random.choice([-1,0,1])
+number_2 =random.choice(list(set([-1,0,1])-set([number_1])))
 
-README_CONTENT_CHECK_FOR = ['__and__' ,
-                            '__or__',
-                            '__repr__',
-                            '__str__',
-                            '__add__',
-                            '__eq__',
-                            '__float__',
-                            '__ge__',
-                            '__gt__',
-                            '__invertsign__',
-                            '__le__',
-                            '__lt__',
-                            '__mul__',
-                            '__sqrt__',
-                            '__bool__'
-                            ]
+README_CONTENT_CHECK_FOR = [
+    '__and__',
+    '__or__',
+    '__repr__',
+    '__str__',
+    '__add__',
+    '__eq__',
+    '__float__',
+    '__ge__',
+    '__gt__',
+    '__invertsign__',
+    '__le__',
+    '__lt__',
+    '__mul__',
+    '__sqrt__',
+    '__bool__'
+]
 
-
-#1
 def test_readme_exists():
     assert os.path.isfile("README.md"), "README.md file missing!"
 
-
-#2
-def test_readme_words_counts():
-    readme = open('README.md','r')
+def test_readme_contents():
+    readme = open("README.md", "r", encoding="utf-8")
     readme_words = readme.read().split()
     readme.close()
-    assert len(readme_words) >= 100 , "Kindly define README properly"
+    #readme_words = [word for line in open('README.md', 'r') for word in line.split()]
+    assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
-
-#3
-def test_readme_proper_desscription():
+def test_readme_proper_description():
     READMELOOKSGOOD = True
-    readme = open('README.md','r')
-    readme_words = readme.read().split()
-    readme.close()
-    for words in README_CONTENT_CHECK_FOR:
-        if words not in readme_words:
+    f = open("README.md", "r", encoding="utf-8")
+    content = f.read()
+    f.close()
+    for c in README_CONTENT_CHECK_FOR:
+        if c not in content:
             READMELOOKSGOOD = False
             pass
-    assert READMELOOKSGOOD == True , "You have not defined all functions/classes in README.md"
+    assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
 
+def test_readme_file_for_formatting():
+    f = open("README.md", "r", encoding="utf-8")
+    content = f.read()
+    f.close()
+    assert content.count("#") >= 10
 
-#4
-def test_readme_for_formatting():
-    readme = open('README.md','r')
-    content = readme.read().split()
-    readme.close()
-    assert content.count('#') >= 5 , "Kindly format the README.md"
-
-
-#5
-def test_identation():
+def test_indentations():
+    ''' Returns pass if used four spaces for each level of syntactically \
+    significant indenting.'''
     lines = inspect.getsource(session4)
-    spaces = re.findall('\n +',lines)
+    spaces = re.findall('\n +.', lines)
     for space in spaces:
         assert len(space) % 4 == 2, "Your script contains misplaced indentations"
         assert len(re.sub(r'[^ ]', '', space)) % 4 == 0, "Your code indentation does not follow PEP8 guidelines" 
 
-
-#6
-def test_funcation_had_cap_letter():
-    functions = inspect.getmembers(session4, inspect.isfunction )
+def test_function_name_had_cap_letter():
+    functions = inspect.getmembers(session4, inspect.isfunction)
     for function in functions:
         assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
 
+def test_invalid_input_valueerror():
+    with pytest.raises(ValueError):
+        q = session4.Qualean(10)
+    with pytest.raises(ValueError):
+        q = session4.Qualean(-1.5)
+    with pytest.raises(ValueError):
+        q = session4.Qualean(5/3)
 
-#7
-def test_repr():
-    assert 'Object at' not in session4.__repr__() , "Kindly return meaningful message from __repr__"
+def test_invalid_input_valueerror_provides_relevant_message():
+    with pytest.raises(ValueError, match=r".*[-1,0,1].*"):
+        q = session4.Qualean(10)
 
+def test_Qualean_repr():
+    r = session4.Qualean(number_1)
+    assert r.__repr__() == f'Qualean Number({r.number})', 'The representation of the Qualean object does not meet expectations'
 
-#8
-def test_str():
-    assert 'Object at' not in session4.__str__() , "Kindly return meaningful message from __str__"
+def test_Qualean_str():
+    r = session4.Qualean(number_1)
+    assert r.__str__() == f'Qualean Number: {r.number}', 'The print of the Qualean object does not meet expectations'
 
+def test_Qualean_comparison():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = session4.Qualean(number_2)  # this will be another random decimal number
+    assert min(r1,r2) < max(r1,r2), "comparison is not working fine"  # using min, max function to be sure which one is smaller
 
+def test_Qualean_comparison_with_non_Qualean():
+    with pytest.raises(NotImplementedError) as e_info:
+        r1 = session4.Qualean(number_1)   # Qualean object
+        r2 = "Qualean"                  # String object
+        r1 < r2
 
-#9 Less than and equals to
-def test_function_exist_check():
-    a = random.uniform(-1,1)
-    assert session4.__lt__(a) , "__lt__ is not implementated"
-    assert session4.__le__(a) , "__le__ is not implementated"
-    assert session4.__eq__(a) , "__eq__ is not implemented"
-    assert session4.__ge__(a) , "__ge__ is not implementated"
-    assert session4.__gt__(a) , " __gt__ is not implementated"
-    assert session4.__mul__(a) , "__mul__ is not implemented"
-    assert session4.__and__(a) , "__and__ is not implemented"
-    assert session4.__or__(a) , "__or__ is not implemented"
-    assert session4.__bool__(a) , "__bool__ is not implementated"
-    assert session4.__float__(a) , "__float__ is not implementated"
-    assert session4.__add__(a) , "__add__ is not implementated"
+def test_Qualean_equality():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = r1  # storing the same object
+    assert r2 == r1, "comparison is not working fine"  # using min, max function to be sure which one is smaller
 
+def test_Qualean_invertsign():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = (-1)*r1.number  # storing the negative of object
+    assert r1.__invertsign__() == r2, "comparison is not working fine"
 
+def test_100_times_sum():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = decimal.Decimal(0)
+    decimal.getcontext().prec = 12
+    for i in range(100):
+        r2 = r2 + r1.number
+    assert 100*r1.number == r2 , "comparison is not working fine "
+    
+def test_1_Million_Qualean_mean_closeto_zero():
+    r2 = decimal.Decimal(0)
+    for i in range(1000000):
+        r1 = session4.Qualean(random.choice([-1,0,1]))  # this will be a random decimal number
+        r2 = r2 + r1.number
+    r2 = r2/1000000
+    assert isclose(decimal.Decimal(0), r2, rel_tol=0.01,abs_tol=0.01), "comparison is not working fine"
 
-#10 NotImplementedError Check 
-def test_notimplementederror_check():
-    with pytest.raises(NotImplementedError):
-        session4.__le__('TSAI') 
-        session4.__lt__('TSAI')
-        session4.__gt__('TSAI')
-        session4.__ge__('TSAI')
-        session4.__eq__('TSAI')
-        session4.__mul__('TSAI')
-        session4.__and__('TSAI')
-        session4.__or__('TSAI')
-        session4.__bool__('TSAI')
-        session4.__float__('TSAI')
-        session4.__add__('TSAI')
-        session4.__sqrt__('TSAI')
+def test_Qualean_BOOL():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    assert r1.__bool__() == bool(r1), "comparison is not working fine"
 
+def test_Qualean_AND():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = session4.Qualean(number_2)  # this will be a random decimal number
+    assert (r1.__and__(r2)) == bool(r1 and r2), "comparison is not working fine"
 
+def test_Qualean_OR():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = session4.Qualean(number_2)  # this will be a random decimal number
+    assert (r1.__or__(r2)) == bool(r1 or r2), "comparison is not working fine"
 
-#11 Decimal sqrt check with class Qualean
-def test_sqrtcheck_with_Decimal():
-    a = random.uniform(-1,1)
-    assert session4.__sqrt__(a) == Decimal(a).sqrt() , "session4.__sqrt__(a) == Decimal(a).sqrt() returns different value"
+def test_Qualean_AND_short_Circuit():
+    r1 = session4.Qualean(0)  # this will be a 0 decimal number
+    # r2 is not defined
+    assert bool(r1 and r2) == False, "comparison is not working fine"
 
+def test_Qualean_OR_short_Circuit():
+    r1 = session4.Qualean(1)  # this will be a random decimal number
+    # r2 is not defined
+    assert bool(r1 or r2) == True, "comparison is not working fine"
 
-#12
+def test_Qualean_ADD():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = session4.Qualean(number_2)  # this will be a random decimal number
+    assert (r1.__add__(r2)) == (r1.number + r2.number), "comparison is not working fine"
+
+def test_Qualean_MUL():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r2 = session4.Qualean(number_2)  # this will be a random decimal number
+    assert (r1.__mul__(r2)) == (r1.number * r2.number), "comparison is not working fine"
+
+def test_Qualean_SQRT():
+    r1 = session4.Qualean(number_1)  # this will be a random decimal number
+    r1.number = abs(r1.number)
+    assert r1.__sqrt__() == r1.number.sqrt(), "comparison is not working fine"
+
+def test_Qualean_SQRT_Negative():
+    r1 = session4.Qualean(1)  # this will be a random decimal number
+    r1.number = abs(r1.number)*(-1)
+    assert type(r1.__sqrt__()) == complex, "comparison is not working fine"
+    
